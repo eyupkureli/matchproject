686fbaf6e1ba18dc0a90b2a4289ed585be72df5e
eyupkureli <eyupkurelii@gmail.com>
2022-01-27 14:17:29 +0000

    projects and gateways data call

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,5 +1,72 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Reports() {
-  return <div></div>;
+  const [projects, setProjects] = useState([]);
+  const [gateways, setGateways] = useState([]);
+
+  const getProjects = () => {
+    axios
+      .get("http://178.63.13.157:8090/mock-api/api/projects")
+      .then((response) => setProjects(response.data.data));
+  };
+
+  const getGateways = () => {
+    axios
+      .get("http://178.63.13.157:8090/mock-api/api/gateways")
+      .then((response) => setGateways(response.data.data));
+  };
+
+  return (
+    <main>
+      <div className="row block center">
+        <div>
+          <h2>Reports</h2>
+          <p className="description">
+            Easily generate a report of your transactions.
+          </p>
+        </div>
+        <table className="buttons">
+          <tr>
+            <td>
+              <button className="color1" onClick={getProjects}>
+                Select project
+              </button>
+            </td>
+            <td>
+              <button className="color1" onClick={getGateways}>
+                Select gateway
+              </button>
+            </td>
+            <td>
+              <button className="color1">From date</button>
+            </td>
+            <td>
+              <button className="color1">To date</button>
+            </td>
+            <td>
+              <button className="color2">Generate report</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div>
+        <table className="projects">
+          {projects.map((pr, i) => (
+            <tr className="row block center">
+              <td>{pr.name}</td>
+              {gateways.map((gt, i) => (
+                <ul>
+                  <il>
+                    <td>{gt.name}</td>
+                  </il>
+                </ul>
+              ))}
+            </tr>
+          ))}
+        </table>
+      </div>
+    </main>
+  );
 }
